dbfa444724ef39717c0468a2956f0b58677c264a
Abhinav <AbhinavSunil@hotmail.com>
2026-08-07 00:04:09 +0530

    Feat : Added thecredential_validator

---
+++ is_Authenticated/utils/nodes.py
@@ -0,0 +1,46 @@
+from state import AuthState
+
+def input_node(state):
+    print(state)
+
+    if state.get('user_name','') == '':
+        username = input('what is your username')
+
+    password = input('Enter your Password')
+
+    if state.get('user_name','') == '':
+        return {"username":username,"password":password}
+    else:
+        return {"password":password}
+
+
+def validate_credentials_node(state):
+
+    username = state.get('user_name','')
+    password = state.get('password','')
+
+    print("Username : ",username," Password : ",password)
+
+    if username == "iamAbhinav01" and password == "HeHEHE":
+        is_Authenticated = True
+    else:
+        is_Authenticated = False
+
+    return {"is_Authenticated : " : is_Authenticated}
+
+auth__state1 :AuthState = {
+    "user_name" : "iamAbhinav01",
+    "password"  : "HeHEHE",
+    "is_Authenticated" : True,
+    "output" : "Login Successful"
+}
+
+auth__state2 :AuthState = {
+    "user_name" : "iamNotAbhinav01",
+    "password"  : "HaHaHaHa",
+    "is_Authenticated" : False,
+    "output" : "Login Failed"
+}
+
+print(validate_credentials_node(auth__state1))
+print(validate_credentials_node(auth__state2))
